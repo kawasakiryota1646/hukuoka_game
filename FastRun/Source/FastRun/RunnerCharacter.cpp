9fc83603c0e0b77939f5cbdbc0e32bd97963f7fc
MSI\GA2B <h1202419005@hiratagakuen.onmicrosoft.com>
2025-11-06 12:49:57 +0900

    no message

--- FastRun/Source/FastRun/RunnerCharacter.cpp
+++ FastRun/Source/FastRun/RunnerCharacter.cpp
@@ -128,7 +128,7 @@ void ARunnerCharacter::StartSlide()
     GetCapsuleComponent()->SetCapsuleHalfHeight(OriginalCapsuleHalfHeight * 0.5f);
 
     // 👇 メッシュを少し上にずらす（見た目用）
-    GetMesh()->AddLocalOffset(FVector(0.f, 0.f, 35.f));
+    GetMesh()->AddLocalOffset(FVector(0.f, 0.f, 40.f));
 
 
     // スライディング時は速度を少し上げる
